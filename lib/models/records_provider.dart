16316e118a7417fe739c1cade0b74231cc248fe5
Mykhailo Keryta <keryta.you@gmail.com>
2021-09-17 23:32:15 +0300

    Add basic sqflite (SQLite) database wrapper

---
+++ lib/models/records_provider.dart
@@ -0,0 +1,87 @@
+import 'package:sqflite/sqflite.dart';
+
+const String tableRecords = 'records';
+const String columnId = '_id';
+const String columnDuration = 'duration';
+const String columnDateTime = 'datetime';
+
+class Record {
+  int? id;
+  late DateTime dateTime;
+  late Duration duration;
+  Record(this.dateTime, this.duration);
+  Map<String, Object?> toMap() {
+    var map = <String, Object?>{
+      columnDateTime: dateTime,
+      columnDuration: duration,
+    };
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+
+  Record.fromMap(Map<String, Object?> map) {
+    var localDateTime = map[columnDateTime] as DateTime?;
+    dateTime = localDateTime!;
+    var localDuration = map[columnDuration] as Duration?;
+    duration = localDuration!;
+    id = map[columnId] as int?;
+  }
+}
+
+class RecordProvider {
+  late Database _db;
+
+  RecordProvider._create();
+
+  static Future<RecordProvider> open(String path) async {
+    var provider = RecordProvider._create();
+    provider._db = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute('''
+            create table $tableRecords ( 
+              $columnId integer primary key autoincrement, 
+              $columnDateTime text not null,
+              $columnDuration integer not null)
+            ''');
+    });
+    return provider;
+  }
+
+  Future<Record> insert(Record record) async {
+    record.id = await _db.insert(tableRecords, record.toMap());
+    return record;
+  }
+
+  Future<Record?> getRecord(int id) async {
+    List<Map<String, Object?>> maps = await _db.query(
+      tableRecords,
+      columns: [columnId, columnDateTime, columnDuration],
+      where: '$columnId = ?',
+      whereArgs: [id],
+    );
+    if (maps.length == 0) return null;
+
+    return Record.fromMap(maps.first);
+  }
+
+  Future<int?> delete(int id) async {
+    return await _db.delete(
+      tableRecords,
+      where: '$columnId = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int?> update(Record record) async {
+    return await _db.update(
+      tableRecords,
+      record.toMap(),
+      where: '$columnId = ?',
+      whereArgs: [record.id],
+    );
+  }
+
+  Future close() async => _db.close();
+}
